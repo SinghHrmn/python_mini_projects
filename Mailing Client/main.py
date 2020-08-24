16ed9b4cab7535c08c989922a842c57df229c5f3
Harmandeep Singh <singh.hrmn98@gmail.com>
2020-08-24 12:11:05 +0530

    Updated main.py

--- Mailing Client/main.py
+++ Mailing Client/main.py
@@ -1,9 +1,8 @@
 import smtplib
 from email import encoders
-from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
-
+from email.mime.text import MIMEText
 
 server = smtplib.SMTP('smtp.gmail.com', 25)
 
@@ -20,3 +19,22 @@ msg = MIMEMultipart()
 msg['From'] = 'Harmandeep Singh'
 msg['To'] = 'recievermail@gmail.com'
 msg['Subject'] = 'Just a Test'
+
+with open('message.txt', 'r') as f:
+    message = f.read()
+
+msg.attach(MIMEText(message , 'plain'))
+
+filename = 'imagename.jpg'
+
+attachment = open(filename, 'rb')
+
+p = MIMEBase('application', 'octet-stream')
+p.set_payload(attachment.read())
+
+encoders.encode_base64(p)
+p.add_header('Content-Disposition', f'attachment; filename={filename}')
+msg.attach(p)
+
+text = msg.as_string()
+server.send_mail('sender_mail@xyz', 'reciever_mail@xyz', text)
