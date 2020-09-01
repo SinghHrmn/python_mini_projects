4874cf077317a943009aea13967498ea17058896
Harmandeep Singh <singh.hrmn98@gmail.com>
2020-09-01 22:19:56 +0530

    Added Ordered Dict

---
+++ Intermediate_Python/Collections/ordered_dict.py
@@ -0,0 +1,16 @@
+from collections import OrderedDict
+
+# Since 3.7 its by default
+
+# defining ordered_dict 
+ordered_dict = OrderedDict()
+ordered_dict['a'] = 1
+ordered_dict['b'] = 2
+ordered_dict['c'] = 3
+ordered_dict['d'] = 4
+ordered_dict['e'] = 5
+ordered_dict['f'] = 6
+
+
+print(ordered_dict)
+# OrderedDict([('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5), ('f', 6)])
